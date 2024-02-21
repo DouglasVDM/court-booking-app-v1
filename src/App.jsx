f8a4f50586ef65041198f0be5f5590945e4d1863
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-21 22:37:04 +0200

    Import fetched data and send to children via props

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,22 @@ import Contact from "./components/Contact";
 import Main from "./components/Main";
 import Projects from "./components/Projects";
 import SideNavbar from "./components/SideNavbar";
-import Work from "./components/Work";
+import Court from "./components/Court";
+
+import useCourts from "./fetchData/useCourts";
+
+const apiEndpointPrefix = import.meta.env.VITE_API_ENDPOINT;
 
 function App() {
+  const { courts } = useCourts(apiEndpointPrefix);
+
   return (
     <div>
       <SideNavbar />
-      <Main/>
-      <Work/>
-      <Projects/>
-      <Contact/>
+      <Main />
+      <Court courts={courts} />
+      <Projects />
+      <Contact courts={courts} />
     </div>
   );
 }
