db600a21ca032be94cc5fb1761d087565be396e9
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2024-02-21 22:34:17 +0200

    Initial booking post with court info

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,18 +1,74 @@
-const Contact = () => {
+import axios from "axios";
+import { useState } from "react";
+
+const apiEndpointPrefix = import.meta.env.VITE_API_ENDPOINT;
+
+const Contact = ({ courts }) => {
+  const [selectedCourt, setSelectedCourt] = useState(null);
+
+  const handleSelectedCourt = (event) => {
+    const selectedCourt = courts.find(
+      (court) => court.court_id === parseInt(event.target.value)
+    );
+    console.log("selectedCourt", selectedCourt.court_name);
+    setSelectedCourt(selectedCourt);
+  };
+
+  const handleSubmit = async () => {
+    const bookingFormData = {
+      court_name: selectedCourt ? selectedCourt.court_name : null,
+      member_id: 1,
+      // selectedMember ? selectedMember.member_id : null,
+      booked_at: new Date().toISOString(),
+      day_name: "Monday",
+      // day_name: selectedDayOfWeek ? selectedDayOfWeek.day_name : null,
+      start_time: "14:00",
+      // start_time: selectedStartTime ? selectedStartTime.start_time : null,
+      duration_hours: 1,
+      booking_type_name: "singles",
+      // duration_hours: selectedDuration ? selectedDuration.duration_hours : null,
+      // booking_type_name: selectedBookingType
+      //   ? selectedBookingType.booking_type_name
+      //   : null,
+    };
+
+    try {
+      const bookingResponse = await axios.post(
+        `${apiEndpointPrefix}/bookings`,
+        bookingFormData,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Booking created:", bookingResponse.data);
+      history.push('/')
+    } catch (err) {
+      console.error("Error creating booking", err.message);
+    }
+  };
+
   return (
     <div id="contact" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
       <h1 className="py-4 text-4xl font-bold text-center text-[#001b5e]">
-        Contact
+        Booking Form
       </h1>
-      <form action="https://getform.io/f/Qe1gRRbJ" method="POST" encType="multipart/form-data">
+      <form action="" method="POST" encType="multipart/form-data">
         <div className="grid md:grid-cols-2 gap-4 w-full py-2">
           <div className="flex flex-col">
-            <label className="uppercase text-sm py-2">Name</label>
-            <input
+            <label className="uppercase text-sm py-2">Select a court</label>
+            <select
               className="border-2 rounded-lg p-3 flex border-gray-300"
-              type="text"
-              name="name"
-            />
+              name="court"
+              onClick={handleSelectedCourt}
+            >
+              {courts.map(({ court_name, court_id }) => (
+                <option value={court_id} key={court_id}>
+                  {court_name}
+                </option>
+              ))}
+            </select>
           </div>
           <div className="flex flex-col">
             <label className="uppercase text-sm py-2">Phone</label>
@@ -47,8 +103,11 @@ const Contact = () => {
             name="message"
           />
         </div>
-        <button className="bg-[#001b5e] text-gray-100 mt-[0.25rem] w-full p-[0.25rem] rounded-lg">
-          Send Message
+        <button
+          className="bg-[#001b5e] text-gray-100 mt-[0.25rem] w-full p-[0.25rem] rounded-lg"
+          onClick={handleSubmit}
+        >
+          Submit
         </button>
       </form>
     </div>
